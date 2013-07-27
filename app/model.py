6a79b45d6ca30a2e6db8d1afea72149a44ba5c09
bessiec <chu.bessie@gmail.com>
2013-07-26 19:09:07 -0700

    7/26/13 7:00pm

--- app/model.py
+++ app/model.py
@@ -1,46 +1,78 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, SmallInteger
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
 
-# ENGINE = None
-# Session = None
-
-engine = create_engine("sqlite:///data.db", echo=True)
+engine = create_engine("sqlite:///ratings.db", echo=True)
 session = scoped_session(sessionmaker(bind= engine,
                                         autocommit= False,
                                         autoflush = False))
+
+
 Base = declarative_base()
-Base.query = db_session.query_property()
+Base.query = session.query_property()
 
-#declaring classes below 
+ROLE_USER = 0
+ROLE_ADMIN = 1
 
-def init_db():
 
-class User(object):
-    id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
-    role = db.Column(db.SmallInteger, default = ROLE_USER)
+class User(Base):
+#users defined on database structure for playshortflix and for Flask megatutorial
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    nickname = Column(String(64), unique = True)
+    email = Column(String(120), unique = True)
+    role = Column(SmallInteger, default = ROLE_USER)
+    posts = relationship('Post', backref = 'author', lazy = 'dynamic')
+    playlists = Column(String(120), unique = False)
+    rating = Column(Integer, unique = False)
+    times_viewed = Column(Integer, unique = False)
+    movies_favorited = Column(Integer, unique = False)
+    bearer_token = Column(String(120), unique = True)
+    twtter = Column(String(120), unique = True)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
-# app = Flask(__name__)
-# app.config.from_object('config')
-# db = SQLAlchemy(app)
+class Post(Base):
+    __tablename__ = "posts"
 
-# from app import views, models
+    id = Column(Integer, primary_key = True)
+    body = Column(String(140), unique = False)
+    timestamp = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-# from app import db
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
 
-# ROLE_USER = 0
-# ROLE_ADMIN = 1
+class Playlists(Base):
+    __tablename__ = "playlists"
 
+    id = Column(Integer, primary_key = True)
+    muliple_movies = Column(Text, unique = False)
+    order_of_plays = Column(Integer, unique = False)
+    playlist_disqus = Column(Text, unique = False)
 
+class Movies(Base):
+    __tablename__ = "movies"
+    url = Column(text)
+    link_type = Column(text)
+    description = Column(text)
+    length = Column(text)
+    source = Column(text)
+    theme = Column(text)
+    festival = Column(text)
+    director = Column(text)
+    product = Column(text)
+    actor = Column(text)
+    movie_disqus = Column(text)
 
-# #fields created as instances of db.Column class, which takes a field type as an argument. 
-# #__repr tells Python how to print objects of this class 
+def main():
+    """ In case this is needed """
+    pass
 
+if __name__ == "__main__":
+    main()
